3c5600758b2d40b17d8dd30a37d891db70867b93
Dexter <gusevsergey85@gmail.comm>
2023-11-15 19:54:43 +0300

    add models and some more views

--- web/urls.py
+++ web/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.welcome_view, name='welcome'),
     path('contacts/', views.contacts, name='get_contacts'),
+    path('fake/product', views.generate_fake_products, name='fake_products'),
+    path('fake/client', views.generate_fake_clients, name='fake_clients'),
+    path('registr/', views.register_client, name='create_client'),
+    path('clients/', views.get_all_clients, name='get_all_clients'),
+    path('client/<int:client_id>/', views.search_client_by_id, name='get_client_by_id'),
+    path('client/delete/<int:client_id>/', views.delete_client, name='delete_client'),
+
 ]
